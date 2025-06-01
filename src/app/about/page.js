4aed23484444226213f4f77c0374bf60bab383ac
alfinsbae0 <108179538+alfinsbae0@users.noreply.github.com>
2025-06-02 03:21:24 +0700

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,7 @@
 import HomeLayout from "../components/home_layout";
 import Splashscreen from "../components/splashscreen";
 import AboutPage from "./about";
+import Splash2 from "../components/splash_2";
 
 export default function About() {
   return (
