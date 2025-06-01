aebcf42ee7e59b62cd10ae96e506c5515c8db98c
alfinsbae0 <108179538+alfinsbae0@users.noreply.github.com>
2025-06-02 03:17:00 +0700

    Update page.js
    change splash screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Homepage from "./home/homepage";
 import HomeLayout from "./components/home_layout";
 import Splashscreen from "./components/splashscreen";
+import Splash2 from "./components/splash_2";
 
 export default function Home() {
   return (
     <>
-      <Splashscreen />
+      // <Splashscreen />
+    <Splash2 />
       <HomeLayout>
         <Homepage />
       </HomeLayout>
